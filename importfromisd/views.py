baf453c3e352b73d12797d2dd656656c751ad043
valery <gugutsidze_vv@rosaski.com>
2021-12-15 16:28:55 +0300

    Добавлены новые камеры

--- importfromisd/views.py
+++ importfromisd/views.py
@@ -27,9 +27,14 @@ def bos(request):
     Cam03Array = [8, 9, 10]
     Cam04Array = [11, 12, 13]
     Cam05Array = [31, 32, 33]
+    Cam06Array = [34, 35]
+    Cam07Array = [27, 28, 29, 30]
+    Cam08Array = [23, 24, 25, 26, 41]
+    Cam11Array = [64, 65, 66, 67]
+    Cam14Array = [58]
     # Определяем есть ли Турникет в списке
     if int(DeviceCode) in Cam01Array or int(DeviceCode) in Cam02Array or int(DeviceCode) in Cam03Array or int(
-            DeviceCode) in Cam04Array or int(DeviceCode) in Cam05Array:
+            DeviceCode) in Cam04Array or int(DeviceCode) in Cam05Array or int(DeviceCode) in Cam06Array or int(DeviceCode) in Cam07Array or int(DeviceCode) in Cam08Array or int(DeviceCode) in Cam11Array or int(DeviceCode) in Cam14Array:
         # Если да, то определяем порт и название камеры
         if int(DeviceCode) in Cam01Array:
             UDP_PORT = 2551
@@ -46,7 +51,21 @@ def bos(request):
         if int(DeviceCode) in Cam05Array:
             UDP_PORT = 2557
             pos_name = "PPS-CAM05 Lift L"
-        
+        if int(DeviceCode) in Cam06Array:
+            UDP_PORT = 2558
+            pos_name = "PPS-CAM06 Lift L"
+        if int(DeviceCode) in Cam07Array:
+            UDP_PORT = 2559
+            pos_name = "PPS-CAM07 Lift E"
+        if int(DeviceCode) in Cam08Array:
+            UDP_PORT = 2560
+            pos_name = "PPS-CAM08 Lift D"
+        if int(DeviceCode) in Cam11Array:
+            UDP_PORT = 2561
+            pos_name = "PPS-CAM11 Lift O"
+        if int(DeviceCode) in Cam14Array:
+            UDP_PORT = 2562
+            pos_name = "PPS-CAM11 Lift G1"
         # Распарсиваем строку даты времени
         DateTime = list(DateTime)
         month = ''.join(DateTime[4:6])
